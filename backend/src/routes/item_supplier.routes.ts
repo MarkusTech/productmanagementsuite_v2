365a5e9ae589bd0bbdc59d20ad58df1f2d0c2d5b
MarkusTech <wm.recopelacion@gmail.com>
2024-10-22 12:54:39 +0800

    Update item_supplier.routes.ts

--- backend/src/routes/item_supplier.routes.ts
+++ backend/src/routes/item_supplier.routes.ts
@@ -1,26 +1,8 @@
 import { Router } from "express";
 import { SupplierController } from "../controller/item_supplierController";
-import { verifyToken } from "../middlewares/authMiddlewares";
+
 const router = Router();
 const supplierController = new SupplierController();
-
-// router.post("/suppliers", verifyToken, supplierController.createSupplier);
-// router.get("/suppliers", verifyToken, supplierController.getAllSuppliers);
-// router.get(
-//   "/suppliers/:supplierID",
-//   verifyToken,
-//   supplierController.getSupplierById
-// );
-// router.put(
-//   "/suppliers/:supplierID",
-//   verifyToken,
-//   supplierController.updateSupplier
-// );
-// router.delete(
-//   "/suppliers/:supplierID",
-//   verifyToken,
-//   supplierController.deleteSupplier
-// );
 
 router.post("/suppliers", supplierController.createSupplier);
 router.get("/suppliers", supplierController.getAllSuppliers);
