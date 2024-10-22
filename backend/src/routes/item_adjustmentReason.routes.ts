4d3867e72e161f9ca65d12a5c81074eb286f55ad
MarkusTech <wm.recopelacion@gmail.com>
2024-10-22 12:52:42 +0800

    Update item_adjustmentReason.routes.ts

--- backend/src/routes/item_adjustmentReason.routes.ts
+++ backend/src/routes/item_adjustmentReason.routes.ts
@@ -1,35 +1,8 @@
 import { Router } from "express";
 import { AdjustmentReasonController } from "../controller/item_adjustmentReasonController";
-import { verifyToken } from "../middlewares/authMiddlewares";
 
 const router = Router();
 const adjustmentReasonController = new AdjustmentReasonController();
-
-// router.post(
-//   "/adjustment-reason",
-//   verifyToken,
-//   adjustmentReasonController.createAdjustmentReason
-// );
-// router.get(
-//   "/adjustment-reason",
-//   verifyToken,
-//   adjustmentReasonController.getAllAdjustmentReasons
-// );
-// router.get(
-//   "/adjustment-reason/:adjustmentReasonID",
-//   verifyToken,
-//   adjustmentReasonController.getAdjustmentReasonById
-// );
-// router.put(
-//   "/adjustment-reason/:adjustmentReasonID",
-//   verifyToken,
-//   adjustmentReasonController.updateAdjustmentReason
-// );
-// router.delete(
-//   "/adjustment-reason/:adjustmentReasonID",
-//   verifyToken,
-//   adjustmentReasonController.deleteAdjustmentReason
-// );
 
 router.post(
   "/adjustment-reason",
