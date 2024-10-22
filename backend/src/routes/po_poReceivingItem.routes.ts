2082bf4a9cd44af1fbdd05d6a0b70b83341317c1
MarkusTech <wm.recopelacion@gmail.com>
2024-10-22 12:55:08 +0800

    Update po_poReceivingItem.routes.ts

--- backend/src/routes/po_poReceivingItem.routes.ts
+++ backend/src/routes/po_poReceivingItem.routes.ts
@@ -1,35 +1,8 @@
 import { Router } from "express";
 import { PoReceivingItemController } from "../controller/po_poReceivingItemController";
-import { verifyToken } from "../middlewares/authMiddlewares";
 
 const router = Router();
 const poReceivingItemController = new PoReceivingItemController();
-
-// router.post(
-//   "/po-receiving-items",
-//   verifyToken,
-//   poReceivingItemController.createPoReceivingItem
-// );
-// router.get(
-//   "/po-receiving-items",
-//   verifyToken,
-//   poReceivingItemController.getAllPoReceivingItems
-// );
-// router.get(
-//   "/po-receiving-items/:poReceivingItemID",
-//   verifyToken,
-//   poReceivingItemController.getPoReceivingItemById
-// );
-// router.put(
-//   "/po-receiving-items/:poReceivingItemID",
-//   verifyToken,
-//   poReceivingItemController.updatePoReceivingItem
-// );
-// router.delete(
-//   "/po-receiving-items/:poReceivingItemID",
-//   verifyToken,
-//   poReceivingItemController.deletePoReceivingItem
-// );
 
 router.post(
   "/po-receiving-items",
