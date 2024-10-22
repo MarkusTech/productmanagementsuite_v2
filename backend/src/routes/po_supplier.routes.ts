a0e500af7bc258f4aaf5c719758ccd5cd0c30634
MarkusTech <wm.recopelacion@gmail.com>
2024-10-22 12:56:09 +0800

    Update po_supplier.routes.ts

--- backend/src/routes/po_supplier.routes.ts
+++ backend/src/routes/po_supplier.routes.ts
@@ -1,28 +1,8 @@
 import { Router } from "express";
 import { PoSupplierController } from "../controller/po_supplierController";
-import { verifyToken } from "../middlewares/authMiddlewares";
 
 const router = Router();
 const poSupplierController = new PoSupplierController();
-
-// Routes
-// router.post("/suppliers", verifyToken, poSupplierController.createSupplier);
-// router.get("/suppliers", verifyToken, poSupplierController.getAllSuppliers);
-// router.get(
-//   "/suppliers/:supplierID",
-//   verifyToken,
-//   poSupplierController.getSupplierById
-// );
-// router.put(
-//   "/suppliers/:supplierID",
-//   verifyToken,
-//   poSupplierController.updateSupplier
-// );
-// router.delete(
-//   "/suppliers/:supplierID",
-//   verifyToken,
-//   poSupplierController.deleteSupplier
-// );
 
 router.post("/suppliers", poSupplierController.createSupplier);
 router.get("/suppliers", poSupplierController.getAllSuppliers);
