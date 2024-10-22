dfb89a9f6383c84748f1040b783e1b46a097052e
MarkusTech <wm.recopelacion@gmail.com>
2024-10-22 16:10:13 +0800

    Update adjustmentReasonService.js

--- frontend/src/services/inventory/adjustmentReasonService.js
+++ frontend/src/services/inventory/adjustmentReasonService.js
@@ -6,10 +6,10 @@ const API_URL = "http://localhost:5001/api/v1/adjustment-reason";
 export const fetchAdjustmentReasons = async () => {
   try {
     const response = await axios.get(API_URL);
-    return response.data.data; // Return the list of adjustment reasons
+    return response.data.data;
   } catch (error) {
     console.error("Error fetching adjustment reasons:", error);
-    throw error; // Throw error to be handled in the component
+    throw error;
   }
 };
 
@@ -17,9 +17,9 @@ export const fetchAdjustmentReasons = async () => {
 export const createAdjustmentReason = async (reasonData) => {
   try {
     const response = await axios.post(API_URL, reasonData);
-    return response.data; // Assuming the response has a success message or data
+    return response.data;
   } catch (error) {
     console.error("Error creating adjustment reason:", error);
-    throw error; // Rethrow to handle it in the component
+    throw error;
   }
 };
