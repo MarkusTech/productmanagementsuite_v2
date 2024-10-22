6ec87ec44f808318feaac1838390e21523660a66
MarkusTech <wm.recopelacion@gmail.com>
2024-10-22 12:52:25 +0800

    Update item_category.routes.ts

--- backend/src/routes/item_category.routes.ts
+++ backend/src/routes/item_category.routes.ts
@@ -1,27 +1,8 @@
 import { Router } from "express";
 import { CategoryController } from "../controller/item_categoryController";
-import { verifyToken } from "../middlewares/authMiddlewares";
 
 const router = Router();
 const categoryController = new CategoryController();
-
-// router.post("/categories", verifyToken, categoryController.createCategory);
-// router.get("/categories", verifyToken, categoryController.getAllCategories);
-// router.get(
-//   "/categories/:categoryID",
-//   verifyToken,
-//   categoryController.getCategoryById
-// );
-// router.put(
-//   "/categories/:categoryID",
-//   verifyToken,
-//   categoryController.updateCategory
-// );
-// router.delete(
-//   "/categories/:categoryID",
-//   verifyToken,
-//   categoryController.deleteCategory
-// );
 
 router.post("/categories", categoryController.createCategory);
 router.get("/categories", categoryController.getAllCategories);
