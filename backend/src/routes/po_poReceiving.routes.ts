f994da3e349969c684a126c16fe9d46d9bb85f88
MarkusTech <wm.recopelacion@gmail.com>
2024-10-22 12:54:52 +0800

    Update po_poReceiving.routes.ts

--- backend/src/routes/po_poReceiving.routes.ts
+++ backend/src/routes/po_poReceiving.routes.ts
@@ -1,35 +1,8 @@
 import { Router } from "express";
 import { PoReceivingController } from "../controller/po_poReceivingController";
-import { verifyToken } from "../middlewares/authMiddlewares";
 
 const router = Router();
 const poReceivingController = new PoReceivingController();
-
-// router.post(
-//   "/po-receiving",
-//   verifyToken,
-//   poReceivingController.createPoReceiving
-// );
-// router.get(
-//   "/po-receiving",
-//   verifyToken,
-//   poReceivingController.getAllPoReceiving
-// );
-// router.get(
-//   "/po-receiving/:poReceivingID",
-//   verifyToken,
-//   poReceivingController.getPoReceivingById
-// );
-// router.put(
-//   "/po-receiving/:poReceivingID",
-//   verifyToken,
-//   poReceivingController.updatePoReceiving
-// );
-// router.delete(
-//   "/po-receiving/:poReceivingID",
-//   verifyToken,
-//   poReceivingController.deletePoReceiving
-// );
 
 router.post("/po-receiving", poReceivingController.createPoReceiving);
 router.get("/po-receiving", poReceivingController.getAllPoReceiving);
