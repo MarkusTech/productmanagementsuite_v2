c7ecf9480816f73c32354110a8f094c61584eaf6
MarkusTech <wm.recopelacion@gmail.com>
2024-10-22 12:53:24 +0800

    Update item_inventoryAdjustment.routes.ts

--- backend/src/routes/item_inventoryAdjustment.routes.ts
+++ backend/src/routes/item_inventoryAdjustment.routes.ts
@@ -5,32 +5,6 @@ import { verifyToken } from "../middlewares/authMiddlewares";
 const router = Router();
 const inventoryAdjustmentController = new InventoryAdjustmentController();
 
-// router.post(
-//   "/inventory-adjustment",
-//   verifyToken,
-//   inventoryAdjustmentController.createInventoryAdjustment
-// );
-// router.get(
-//   "/inventory-adjustment",
-//   verifyToken,
-//   inventoryAdjustmentController.getAllInventoryAdjustments
-// );
-// router.get(
-//   "/inventory-adjustment/:adjustmentID",
-//   verifyToken,
-//   inventoryAdjustmentController.getInventoryAdjustmentById
-// );
-// router.put(
-//   "/inventory-adjustment/:adjustmentID",
-//   verifyToken,
-//   inventoryAdjustmentController.updateInventoryAdjustment
-// );
-// router.delete(
-//   "/inventory-adjustment/:adjustmentID",
-//   verifyToken,
-//   inventoryAdjustmentController.deleteInventoryAdjustment
-// );
-
 router.post(
   "/inventory-adjustment",
   inventoryAdjustmentController.createInventoryAdjustment
