578e1cbe70c3ca76141d05d59ddf7d0c3815693d
MarkusTech <wm.recopelacion@gmail.com>
2024-10-22 12:53:12 +0800

    Update item_inventory.routes.ts

--- backend/src/routes/item_inventory.routes.ts
+++ backend/src/routes/item_inventory.routes.ts
@@ -1,27 +1,8 @@
 import { Router } from "express";
 import { InventoryController } from "../controller/item_inventoryController";
-import { verifyToken } from "../middlewares/authMiddlewares";
 
 const router = Router();
 const inventoryController = new InventoryController();
-
-// router.post("/inventory", verifyToken, inventoryController.createInventory);
-// router.get("/inventory", verifyToken, inventoryController.getAllInventory);
-// router.get(
-//   "/inventory/:inventoryID",
-//   verifyToken,
-//   inventoryController.getInventoryById
-// );
-// router.put(
-//   "/inventory/:inventoryID",
-//   verifyToken,
-//   inventoryController.updateInventory
-// );
-// router.delete(
-//   "/inventory/:inventoryID",
-//   verifyToken,
-//   inventoryController.deleteInventory
-// );
 
 router.post("/inventory", inventoryController.createInventory);
 router.get("/inventory", inventoryController.getAllInventory);
