07796470e100a8097a457da2b481a7c7aea59c14
MarkusTech <wm.recopelacion@gmail.com>
2024-10-22 12:55:26 +0800

    Update po_purchaseOrder.routes.ts

--- backend/src/routes/po_purchaseOrder.routes.ts
+++ backend/src/routes/po_purchaseOrder.routes.ts
@@ -1,39 +1,8 @@
 import { Router } from "express";
 import { PurchaseOrderController } from "../controller/po_purchaseOrderController";
-import { verifyToken } from "../middlewares/authMiddlewares";
 
 const router = Router();
 const purchaseOrderController = new PurchaseOrderController();
-
-// router.post(
-//   "/purchase-orders",
-//   verifyToken,
-//   purchaseOrderController.createPurchaseOrder
-// );
-
-// router.get(
-//   "/purchase-orders",
-//   verifyToken,
-//   purchaseOrderController.getAllPurchaseOrders
-// );
-
-// router.get(
-//   "/purchase-orders/:poID",
-//   verifyToken,
-//   purchaseOrderController.getPurchaseOrderById
-// );
-
-// router.put(
-//   "/purchase-orders/:poID",
-//   verifyToken,
-//   purchaseOrderController.updatePurchaseOrder
-// );
-
-// router.delete(
-//   "/purchase-orders/:poID",
-//   verifyToken,
-//   purchaseOrderController.deletePurchaseOrder
-// );
 
 router.post("/purchase-orders", purchaseOrderController.createPurchaseOrder);
 
