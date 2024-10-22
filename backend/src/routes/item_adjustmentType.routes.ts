edaeb23a605f0956148dae6f841e54130f6ff0a6
MarkusTech <wm.recopelacion@gmail.com>
2024-10-22 12:52:55 +0800

    Update item_adjustmentType.routes.ts

--- backend/src/routes/item_adjustmentType.routes.ts
+++ backend/src/routes/item_adjustmentType.routes.ts
@@ -1,35 +1,8 @@
 import { Router } from "express";
 import { AdjustmentTypeController } from "../controller/item_adjustmentTypeController";
-import { verifyToken } from "../middlewares/authMiddlewares";
 
 const router = Router();
 const adjustmentTypeController = new AdjustmentTypeController();
-
-// router.post(
-//   "/adjustment-type",
-//   verifyToken,
-//   adjustmentTypeController.createAdjustmentType
-// );
-// router.get(
-//   "/adjustment-type",
-//   verifyToken,
-//   adjustmentTypeController.getAllAdjustmentTypes
-// );
-// router.get(
-//   "/adjustment-type/:adjustmentTypeID",
-//   verifyToken,
-//   adjustmentTypeController.getAdjustmentTypeById
-// );
-// router.put(
-//   "/adjustment-type/:adjustmentTypeID",
-//   verifyToken,
-//   adjustmentTypeController.updateAdjustmentType
-// );
-// router.delete(
-//   "/adjustment-type/:adjustmentTypeID",
-//   verifyToken,
-//   adjustmentTypeController.deleteAdjustmentType
-// );
 
 router.post("/adjustment-type", adjustmentTypeController.createAdjustmentType);
 router.get("/adjustment-type", adjustmentTypeController.getAllAdjustmentTypes);
